93ae9da6ccb60f14a3bdbc70080e3416cd12dcbb
BorisD2023 <borisd7@web.de>
2023-06-05 16:59:26 +0200

    update

---
+++ BananaBank_ReactJS/src/components/Header.jsx
@@ -0,0 +1,17 @@
+
+const Header = () =>{
+    return (
+        
+            <header>
+            <section>
+                <img id="logoImg" src="./banana-bg.gif" alt="" />
+            </section>
+            <section className="header">
+            <h1 id="title">Banana Bank</h1>
+            </section>
+            </header>
+    
+    )
+}
+
+export default Header
---
+++ src/components/MainSection.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+import Button from "./Button"
+
+
+const MainSection = () =>{
+
+    const [input, setInput] = useState('')
+    console.log(input);
+    const [guthaben, setGuthaben] = useState(0)
+
+    return(
+        
+        <main className="konto">
+            <h2>Dein Girokonto</h2>
+            <section className="saldo">{guthaben}€</section>
+            <section>
+                <input onChange={(event) => setInput(Number(event.target.value))} className="geldbetrag" type="number" placeholder="Gib einen Geldbetrag ein"/>
+            </section>
+            <article>
+                <button onClick={() => {setGuthaben((prev) => prev + Number(input)), setInput('')}} className="einzahlen">Einzahlen</button>
+                <button onClick={() => {setGuthaben((prev) => prev - Number(input)), setInput('')}} className="einzahlen">Auszahlen</button>
+            </article>
+        </main>
+        
+    )
+}
+
+export default MainSection
